1acb0fb5ff5a83e787511bd2415fb0cf790e2862
jaehuiui <ljh9081@kaist.ac.kr>
2022-06-12 16:00:53 +0900

    Fix : Local Env

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -30,7 +30,7 @@ interface Props {
 const Button = ({ req, setRes }: Props) => {
   const detection = async () => {
     try {
-      const res = await axios.get(`https://cs376-server.herokuapp.com/model?data=${req}`);
+      const res = await axios.get(`http://localhost:5000/model?data=${req}`);
 
       console.log(res);
 
